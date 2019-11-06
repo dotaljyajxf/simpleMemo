3aeb11b1108d6bcd5f00ccc2bc61070482110e67
dataljyajxf <542730772@qq.com>
2019-11-06 14:04:39 +0800

    add pb

--- src/firstWeb/models/rpc_auto.go
+++ src/firstWeb/models/rpc_auto.go
@@ -1,1 +1,47 @@
+//DO NOT MODIFY GENERATED CODE !!!
+//DO NOT MODIFY GENERATED CODE !!!
+//DO NOT MODIFY GENERATED CODE !!!
+
 package models
+import (
+	"firstWeb/proto/pb"
+	"github.com/golang/protobuf/proto"
+)
+
+var gRpcMethodMap map[string]func(args []byte) (interface{},error)
+
+func init() {
+	gRpcMethodMap = map[string]func(args []byte) (interface{} ,error) {
+		"auth.GetAuthInfo": proxy_auth_GetAuthInfo,
+		
+	}
+}
+
+
+func proxy_auth_GetAuthInfo(args []byte) (interface{} ,error) {
+	
+	request := pb.NewTGetAuthArg()
+	defer request.Put()
+
+	response := pb.NewTAuthInfo()
+	defer response.Put()
+	
+	err := decodePb(args, request)
+	if err != nil {
+		return nil,	errors.New("decodeArgs error")
+
+	}
+	
+	err := auth.GetAuthInfo(request,response)
+	return response,err
+}
+
+
+func decodePb(msg []byte  ,obj interface{}) error {
+	pMsg,ok := obj.(proto.Message)
+	if !ok {
+		return errors.New("type error")
+	}
+
+	return proto.Unmarshal(msg,pMsg)
+}
