6c2aec34254fae63b1227dfafd891e1644cc008c
dataljyajxf <542730772@qq.com>
2019-11-06 14:23:56 +0800

    add rpc_auto

--- src/firstWeb/models/rpc_auto.go
+++ src/firstWeb/models/rpc_auto.go
@@ -3,54 +3,54 @@
 //DO NOT MODIFY GENERATED CODE !!!
 
 package models
-
 import (
-	"errors"
-	"firstWeb/models/auth"
 	"firstWeb/proto/pb"
 	"github.com/golang/protobuf/proto"
 	"github.com/sirupsen/logrus"
+	"errors"
+    "firstWeb/models/auth"
+	
 )
 
-var gRpcMethodMap map[string]func(args []byte) (interface{}, error)
 
-func init() {
-	gRpcMethodMap = map[string]func(args []byte) (interface{}, error){
+var gRpcMethodMap = map[string]func(args []byte) (interface{} ,error) {
 		"auth.GetAuthInfo": proxy_auth_GetAuthInfo,
-	}
+		
 }
 
-func DoRpcMethod(method string, arg []byte) (interface{}, error) {
-	if f, ok := gRpcMethodMap[method]; ok {
-		return f(arg)
-	}
-	logrus.Fatalf("unknow method %s", method)
-	return nil, errors.New("unknow method")
-}
 
-func proxy_auth_GetAuthInfo(args []byte) (interface{}, error) {
-
+func proxy_auth_GetAuthInfo(args []byte) (interface{} ,error) {
+	
 	request := pb.NewTGetAuthArg()
 	defer request.Put()
 
 	response := pb.NewTAuthInfo()
 	defer response.Put()
-
+	
 	err := decodePb(args, request)
 	if err != nil {
-		return nil, errors.New("decodeArgs error")
+		return nil,	errors.New("decodeArgs error")
 
 	}
-
-	err = auth.GetAuthInfo(request, response)
-	return response, err
+	
+	err = auth.GetAuthInfo(request,response)
+	return response,err
 }
 
-func decodePb(msg []byte, obj interface{}) error {
-	pMsg, ok := obj.(proto.Message)
+
+func decodePb(msg []byte  ,obj interface{}) error {
+	pMsg,ok := obj.(proto.Message)
 	if !ok {
 		return errors.New("type error")
 	}
 
-	return proto.Unmarshal(msg, pMsg)
+	return proto.Unmarshal(msg,pMsg)
+}
+
+func DoRpcMethod(method string,arg []byte) (interface{} ,error){
+	if f ,ok := gRpcMethodMap[method];ok {
+		return f(arg)
+	}
+	logrus.Fatalf("unknow method %s",method)
+	return nil,errors.New("unknow method")
 }
