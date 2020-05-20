6de2c425f14e55e92359398a8d98c410dec47d11
dataljyajxf <542730772@qq.com>
2020-05-20 18:33:40 +0800

    add modify pb

--- src/firstWeb/module/rpc_auto.go
+++ src/firstWeb/module/rpc_auto.go
@@ -3,48 +3,52 @@
 //DO NOT MODIFY GENERATED CODE !!!
 
 package module
-
 import (
-	"errors"
-	"firstWeb/module/auth"
 	"firstWeb/proto/pb"
 	"github.com/golang/protobuf/proto"
+	"errors"
+    "firstWeb/module/auth"
+	
 )
 
-var gRpcMethodMap = map[string]func(args []byte) (interface{}, error){
-	"auth.GetAuthInfo": proxy_auth_GetAuthInfo,
+
+var gRpcMethodMap = map[string]func(args []byte) (interface{} ,error) {
+		"auth.GetAuthInfo": proxy_auth_GetAuthInfo,
+		
 }
 
-func proxy_auth_GetAuthInfo(args []byte) (interface{}, error) {
 
+func proxy_auth_GetAuthInfo(args []byte) (interface{} ,error) {
+	
 	request := pb.NewTAuthLoginArg()
 	defer request.Put()
 
 	response := pb.NewTAuthInfo()
 	defer response.Put()
-
+	
 	err := decodePb(args, request)
 	if err != nil {
-		return nil, errors.New("decodeArgs error")
+		return nil,	errors.New("decodeArgs error")
 
 	}
-
-	err = auth.GetAuthInfo(request, response)
-	return response, err
+	
+	err = auth.GetAuthInfo(request,response)
+	return response,err
 }
 
-func decodePb(msg []byte, obj interface{}) error {
-	pMsg, ok := obj.(proto.Message)
+
+func decodePb(msg []byte  ,obj interface{}) error {
+	pMsg,ok := obj.(proto.Message)
 	if !ok {
 		return errors.New("type error")
 	}
 
-	return proto.Unmarshal(msg, pMsg)
+	return proto.Unmarshal(msg,pMsg)
 }
 
-func DoRpcMethod(method string, arg []byte) (interface{}, error) {
-	if f, ok := gRpcMethodMap[method]; ok {
+func DoRpcMethod(method string,arg []byte) (interface{} ,error){
+	if f ,ok := gRpcMethodMap[method];ok {
 		return f(arg)
 	}
-	return nil, errors.New("unknow method")
+	return nil,errors.New("unknow method")
 }
